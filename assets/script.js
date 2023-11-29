f905768209f3a6d40d66e559680668bc513dffb0
Igho Manny Ibuje <imigreat@yahoo.com>
2023-11-29 01:30:27 +0000

    add Javascript to project

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var topic = "HTML, CSS, Git, JavaScript";
+console.log(topic);
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
+
+var shapes = ["Triangle", "Square", "Pentagon", "Circle"];
+//console.log(shapes[0]);
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
